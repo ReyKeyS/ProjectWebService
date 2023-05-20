dcafe3037197a0ec4872618b4c9462aad9b4dbec
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2023-05-20 19:43:52 +0700

    addShipping

--- src/controllers/developer.js
+++ src/controllers/developer.js
@@ -1,6 +1,11 @@
 // Req
+require("dotenv").config();
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const crypto = require("crypto");
 const bcrypt = require("bcrypt");
+const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const db = require("../models/index");
 const conn = db.sequelize
@@ -241,23 +246,169 @@ const getCityParams = async (req, res) => {
     const { city_id } = req.params
 
     // Query
-    const city = await cities.findByPk(city_id)
-    console.log(city);
+    const city = await cities.findOne({
+        attributes: [
+            "city_id",
+            "name",
+            [Sequelize.literal("province.name"), "Province"],
+            "latitude",
+            "longitude",
+            "postal_code",
+        ],
+        include: [
+            { model: provinces, attributes: []}
+        ],
+        where: { city_id: city_id }
+    })
     if (city == null) return res.status(404).send({message: "City not found"})
 
-    return res.status(200).send({
-        city_id: city_id,
-        name: city.name,
-        latitude: city.latitude,
-        longitude: city.longitude,
-        postal_code: city.postal_code
-    })
+    return res.status(200).send(city)
 }
 
 
-const getEstimate = async (req, res) => {}
+const getEstimate = async (req, res) => {
+    const user = await users.findByPk(req.user.user_id)
 
-const addShipping = async (req, res) => {}
+    const { city_from, city_to, weight } = req.query
+
+    // Origin
+    const city_origin = await cities.findByPk(city_from)
+    if (city_origin == null) return res.status(404).send({message: "City not found"})
+
+    // Destination
+    const city_destination = await cities.findByPk(city_to)
+    if (city_destination == null) return res.status(404).send({message: "City not found"})
+
+    const opts = { headers: { key: process.env.RAJAONGKIR_KEY }}
+    // JNE
+    const resultJNE = await axios.post(
+        "https://api.rajaongkir.com/starter/cost",
+        { origin: city_origin.raja_id_city, destination: city_destination.raja_id_city, weight: weight, courier: "jne" },
+        opts
+    )
+
+    // POS
+    const resultPOS = await axios.post(
+        "https://api.rajaongkir.com/starter/cost",
+        { origin: city_origin.raja_id_city, destination: city_destination.raja_id_city, weight: weight, courier: "pos" },
+        opts
+    )
+    
+    // TIKI
+    const resultTIKI = await axios.post(
+        "https://api.rajaongkir.com/starter/cost",
+        { origin: city_origin.raja_id_city, destination: city_destination.raja_id_city, weight: weight, courier: "tiki" },
+        opts
+    )
+
+    // Weather
+    const weatherOrigin = await axios({
+        method: "get",
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${city_origin.latitude}&lon=${city_origin.longitude}&appid=${process.env.OPENWEATHER_KEY}`
+    })
+    const weatherDestination = await axios({
+        method: "get",
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${city_destination.latitude}&lon=${city_destination.longitude}&appid=${process.env.OPENWEATHER_KEY}`
+    })
+
+    return res.status(200).json({
+        estimate: { 
+            city_origin: resultJNE.data.rajaongkir.origin_details.city_name,
+            city_destination: resultJNE.data.rajaongkir.destination_details.city_name,
+            weight: resultJNE.data.rajaongkir.query.weight + " gram",
+        },
+        weather_city_origin: weatherOrigin.data,
+        weather_city_destination: weatherDestination.data,
+        courier: {
+            JNE: resultJNE.data.rajaongkir.results[0],
+            POS: resultPOS.data.rajaongkir.results[0],
+            TIKI: resultTIKI.data.rajaongkir.results[0],
+        }   
+    })
+}
+
+const addShipping = async (req, res) => {
+    const shippingKe = await shippings.count()
+    req.gmbrCount = parseInt(shippingKe) + 1
+
+    const uploadingFile = await upload.single("picture")
+    uploadingFile(req, res, async (err) => {
+        if (err) {
+            console.log(err);
+            return res
+                .status(400)
+                .send((err.message || err.code) + " pada field " + err.field);
+        }
+
+        const { city_from, city_to, weight, courier, service, notes } = req.body
+        
+        // Origin
+        const city_origin = await cities.findByPk(city_from)
+        if (city_origin == null) return res.status(400).send({message: "City not found"})
+
+        // Destination
+        const city_destination = await cities.findByPk(city_to)
+        if (city_destination == null) return res.status(400).send({message: "City not found"})
+
+        // Cek courier
+        if (courier.toLowerCase() == "jne"){
+            if (service.toLowerCase() != "oke" && service.toLowerCase() != "reg" && service.toLowerCase() != "yes") return res.status(400).send({message: "Invalid Service"})
+        }else if (courier.toLowerCase() == "pos"){
+            if (service.toLowerCase() != "pos reguler" && service.toLowerCase() != "pos nextday") return res.status(400).send({message: "Invalid Service"})
+        }else if (courier.toLowerCase() == "tiki"){
+            if (service.toLowerCase() != "eco" && service.toLowerCase() != "reg" && service.toLowerCase() != "ons") return res.status(400).send({message: "Invalid Service"})
+        }else return res.status(400).send({message: "Invalid Courier"})
+
+        // Raja Ongkir
+        const opts = { headers: { key: process.env.RAJAONGKIR_KEY }}
+        const resultOngkos = await axios.post(
+            "https://api.rajaongkir.com/starter/cost",
+            { origin: city_origin.raja_id_city, destination: city_destination.raja_id_city, weight: weight, courier: courier.toLowerCase() },
+            opts
+        )
+
+        // OpenWeather
+        const weatherOrigin = await axios({
+            method: "get",
+            url: `https://api.openweathermap.org/data/2.5/weather?lat=${city_origin.latitude}&lon=${city_origin.longitude}&appid=${process.env.OPENWEATHER_KEY}`
+        })
+        const weatherDestination = await axios({
+            method: "get",
+            url: `https://api.openweathermap.org/data/2.5/weather?lat=${city_destination.latitude}&lon=${city_destination.longitude}&appid=${process.env.OPENWEATHER_KEY}`
+        })
+
+        // Generate ID
+        let newID = "SP"
+        const currMax = await shippings.count({ where: { shipping_id: { [Op.like]: "SP%" } } })
+        newID += (parseInt(currMax) + 1).toString().padStart(3, '0');
+
+        let serv = resultOngkos.data.rajaongkir.results[0].costs
+        let cost, est;
+        for (const x of serv) {
+            if (x.service.toLowerCase() == service.toLowerCase()){
+                cost = x.cost[0].value;
+                est = x.cost[0].etd;
+                break;
+            }
+        }
+
+        await shippings.create({
+            shipping_id: newID,
+            city_from: city_from,
+            city_to: city_to,
+            status: "Pending",
+            cost: cost,
+            weight: weight,
+            estimate_day: est,
+            distance: 3,
+            foto_barang: `barang_${req.gmbrCount}${req.fileExt}`
+        })
+
+        return res.status(200).send({
+            message: "Berhasil menambah Shipping"
+        })
+    });
+}
 
 const deleteShipping = async (req, res) => {}
 
@@ -276,3 +427,57 @@ module.exports = {
     addShipping,
     deleteShipping,
 }
+
+
+// Function multer
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        const folderName = `uploads/Bukti_Shipping`;
+
+        if (!fs.existsSync(folderName)) {
+            fs.mkdirSync(folderName, { recursive: true });
+        }
+
+        callback(null, folderName);
+    },
+    filename: (req, file, callback) => {
+        console.log(file);
+        const fileExtension = path.extname(file.originalname).toLowerCase();
+
+        if (file.fieldname == "picture") {
+            callback(null, `barang_${req.gmbrCount}${fileExtension}`);
+        } else {
+            callback(null, false);
+        }
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 10000000, // 10mb
+    },
+    fileFilter: (req, file, callback) => {
+        // buat aturan dalam bentuk regex, mengenai extension apa saja yang diperbolehkan
+        const rules = /jpeg|jpg|png/;
+
+        const fileExtension = path.extname(file.originalname).toLowerCase();
+        const fileMimeType = file.mimetype;
+
+        const cekExt = rules.test(fileExtension);
+        const cekMime = rules.test(fileMimeType);
+
+        if (cekExt && cekMime) {
+            req.fileExt = fileExtension
+            callback(null, true);
+        } else {
+            callback(null, false);
+            return callback(
+                new multer.MulterError(
+                    "Tipe file harus .jpg, .jpeg, atau .png",
+                    file.fieldname
+                )
+            );
+        }
+    },
+});
