5120610b939929d93b7aeb05413ec32c8875762d
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2023-05-26 22:46:01 +0700

    Rapino Courier

--- src/controllers/courier.js
+++ src/controllers/courier.js
@@ -12,119 +12,6 @@ const schema = require("../utils/validation/index");
 // Models
 const { users, cities, shippings } = require("../models");
 
-// Function multer
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        const folderName = `uploads/ProfPic_Kurir`;
-
-        if (!fs.existsSync(folderName)) {
-            fs.mkdirSync(folderName, { recursive: true });
-        }
-
-        callback(null, folderName);
-    },
-    filename: (req, file, callback) => {
-        console.log(file);
-        const fileExtension = path.extname(file.originalname).toLowerCase();
-        
-        if (file.fieldname == "profpic") {
-            let namaFile = `profpic_${req.body.username}${fileExtension}`;
-            req.namaFile = namaFile
-            callback(null, namaFile);
-        } else {
-            callback(null, false);
-        }
-    },
-});
-
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 10000000, // 10mb
-    },
-    fileFilter: (req, file, callback) => {
-        // buat aturan dalam bentuk regex, mengenai extension apa saja yang diperbolehkan
-        const rules = /jpeg|jpg|png/;
-
-        const fileExtension = path.extname(file.originalname).toLowerCase();
-        const fileMimeType = file.mimetype;
-
-        const cekExt = rules.test(fileExtension);
-        const cekMime = rules.test(fileMimeType);
-
-        if (cekExt && cekMime) {
-            callback(null, true);
-        } else {
-            callback(null, false);
-            return callback(
-                new multer.MulterError(
-                    "Tipe file harus .jpg, .jpeg, atau .png",
-                    file.fieldname
-                )
-            );
-        }
-    },
-});
-
-// const registerCourier = async (req, res) => {
-// console.log(req.body);
-// const { username, password, confirm_password, display_name, no_telp, foto_ktp } = req.body
-// // Validation
-// try {
-//     let result = await schema.registerCourSchema.validateAsync(req.body, {
-//         abortEarly: false,
-//     });
-// } catch (error) {
-//     const processedResult = error.details.reduce((hasil, item) => {
-//         const key = item.context.key || item.context.main;
-//         if (key in hasil) {
-//             hasil[key].push(item.message);
-//         } else {
-//             hasil[key] = [item.message];
-//         }
-//         return hasil;
-//     }, {});
-//     return res.status(400).json({ msg: "Validasi gagal", payload: processedResult });
-// }
-
-// // Cek username available
-// const cekUsername = await users.findOne({ where: { username: username } })
-// if (cekUsername != null) return res.status(400).send({ message: "Username sudah terpakai" })
-
-// // Password
-// let hashedPassword;
-// await bcrypt.hash(password, 10).then((hash) => {
-//     hashedPassword = hash;
-// });
-
-// // Generate ID
-// let newID = "US"
-// const currMax = await users.count({ where: { user_id: { [Op.like]: "US%" } } })
-// newID += (parseInt(currMax) + 1).toString().padStart(3, '0');
-// Upload KTP
-// const uploadingFile =await upload.single("foto_ktp");
-// uploadingFile(req, res, (err) => {
-//     const body = req.body;
-//     return res.status(200).json(body);
-// });
-
-// const add = await users.create({
-//     user_id: newID,
-//     username: username,
-//     password: hashedPassword,
-//     display_name: display_name,
-//     no_telp: no_telp,
-//     roles: "cour"
-// })
-
-// return res.status(201).send({
-//     message: "Berhasil Register", data: {
-//         username: username,
-//         display_name: display_name,
-//         no_telp: no_telp
-//     }
-// })
-// }
 
 const registerCourier = async (req, res) => {
     // Upload KTP
@@ -358,3 +245,58 @@ module.exports = {
     takeOrder,
     cancelShipping
 }
+
+
+// Function multer
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        const folderName = `uploads/ProfPic_Kurir`;
+
+        if (!fs.existsSync(folderName)) {
+            fs.mkdirSync(folderName, { recursive: true });
+        }
+
+        callback(null, folderName);
+    },
+    filename: (req, file, callback) => {
+        console.log(file);
+        const fileExtension = path.extname(file.originalname).toLowerCase();
+        
+        if (file.fieldname == "profpic") {
+            let namaFile = `profpic_${req.body.username}${fileExtension}`;
+            req.namaFile = namaFile
+            callback(null, namaFile);
+        } else {
+            callback(null, false);
+        }
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 10000000, // 10mb
+    },
+    fileFilter: (req, file, callback) => {
+        // buat aturan dalam bentuk regex, mengenai extension apa saja yang diperbolehkan
+        const rules = /jpeg|jpg|png/;
+
+        const fileExtension = path.extname(file.originalname).toLowerCase();
+        const fileMimeType = file.mimetype;
+
+        const cekExt = rules.test(fileExtension);
+        const cekMime = rules.test(fileMimeType);
+
+        if (cekExt && cekMime) {
+            callback(null, true);
+        } else {
+            callback(null, false);
+            return callback(
+                new multer.MulterError(
+                    "Tipe file harus .jpg, .jpeg, atau .png",
+                    file.fieldname
+                )
+            );
+        }
+    },
+});
